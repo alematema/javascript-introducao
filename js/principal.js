51ad28e88661d639bbe5bc44d257bb25f965df49
alematema <alematema@gmail.com>
2018-03-21 16:05:28 -0300

    busca do primeiro paciente e validacoes de seus dados antes de calcular IMC && add css para mostrar celulas com dados invalidos na tabela de pacientes

--- js/principal.js
+++ js/principal.js
@@ -10,10 +10,37 @@ titulo.textContent = 'Edna Nutricionista';
   //recupera Altura
     var altura = document.querySelector('#primeiro-paciente').querySelector('.info-altura').textContent;
     console.log(altura);
-  //calcula imc (https://pt.wikipedia.org/wiki/Índice_de_massa_corporal#Cálculo)
-    imc = massa/(altura*altura);
-    console.log('imc ' + imc);
-//fim Calculo
 
-//exibe imc na linha correspondente
-    document.querySelector('#primeiro-paciente').querySelector('.info-imc').textContent = imc;
+
+    //validacoes
+    var pesoEhValido = true; // assumindo de boa fé que o peso é válido
+    var alturaEhValida = true; // assumindo de boa fé que a altura é válida
+
+    if(massa <=0 || massa >= 1000) {
+       pesoEhValido = false;
+    }
+
+    if(altura <= 0 || altura >= 3.00) {
+      alturaEhValida = false;
+    }
+
+    if(!pesoEhValido && !alturaEhValida){
+        document.querySelector('#primeiro-paciente').querySelector('.info-imc').textContent='massa e altura inválida';
+        document.querySelector('#primeiro-paciente').querySelector('.info-peso').className='bad-info';
+        document.querySelector('#primeiro-paciente').querySelector('.info-altura').className='bad-info';
+    }else if(!pesoEhValido){
+        document.querySelector('#primeiro-paciente').querySelector('.info-imc').textContent='massa inválida';
+        document.querySelector('#primeiro-paciente').querySelector('.info-peso').className='bad-info';
+    }else if(!alturaEhValida){
+        document.querySelector('#primeiro-paciente').querySelector('.info-imc').textContent='altura inválida';
+        document.querySelector('#primeiro-paciente').querySelector('.info-altura').className='bad-info';
+    }else{
+
+      //tudo ok, entao
+      //calcula imc (https://pt.wikipedia.org/wiki/Índice_de_massa_corporal#Cálculo)
+        imc = massa/(altura*altura);
+        console.log('imc ' + imc);
+        //exibe imc na linha correspondente
+        document.querySelector('#primeiro-paciente').querySelector('.info-imc').textContent = imc;
+    }
+//fim Calculo IMC
